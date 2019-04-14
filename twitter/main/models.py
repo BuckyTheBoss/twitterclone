8c8a823f85aa894dee60beba7bedea367f88d573
Avi <buchman.avi@gmail.com>
2019-04-14 11:30:40 +0300

    initial twitter commit

---
+++ twitter/main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	img = models.URLField(default=None, null=True, blank=True)
+	bio = models.TextField(max_length=500, blank=True)
+	birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Tweet(models.Model):
+	content = models.CharField(max_length=200)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(default=timezone.now)
+
+class TweetComment(models.Model):
+	content = models.CharField(max_length=200)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(default=timezone.now)
+	tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
